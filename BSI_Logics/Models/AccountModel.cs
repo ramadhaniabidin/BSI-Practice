5bceee413af6be9d94c0293f832a308645be0f6b
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-08-28 11:57:40 +0700

    add LoginController

---
+++ BSI_Logics/Models/AccountModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSI_Logics.Models
+{
+    public class AccountModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int role_id { get; set; }
+        public string email { get; set; }
+        public string department { get; set; }
+        public string role { get; set; }
+    }
+}

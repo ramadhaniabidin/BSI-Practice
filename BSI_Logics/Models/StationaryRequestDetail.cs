00904d30d37bc11c7533ab0062650d64483a1227
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-08-28 11:43:45 +0700

    add StationaryRequestDetail model

---
+++ BSI_Logics/Models/StationaryRequestDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSI_Logics.Models
+{
+    public class StationaryRequestDetail
+    {
+        public int id { get; set; }
+        public int header_id { get; set; }
+        public int no { get; set; }
+        public string item_name { get; set; }
+        public string uom { get; set; }
+        public int stock { get; set; }
+        public int request_qty { get; set; }
+        public string reason { get; set; }
+    }
+}

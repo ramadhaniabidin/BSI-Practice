d88e6cc71d39c18d2e6b2408a68611deb06d5fc6
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-08-28 13:26:53 +0700

    add model for Login

--- BSI_Logics/Controller/LoginController.cs
+++ BSI_Logics/Controller/LoginController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BSI_Logics.Common;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,13 @@ namespace BSI_Logics.Controller
 {
     public class LoginController
     {
-
+        DatabaseManager db = new DatabaseManager();
+        SqlConnection conn = new SqlConnection();
+        
+        public string GetLoginToken()
+        {
+            var returnedOutput = "";
+            return returnedOutput;
+        }
     }
 }

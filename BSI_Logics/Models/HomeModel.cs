7d8fc968a86a8684932bbfeff4c8ff017f8b4d8e
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-10-28 23:30:47 +0700

    add HomeModel

---
+++ BSI_Logics/Models/HomeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSI_Logics.Models
+{
+    public class HomeModel
+    {
+        public int id { get; set; }
+        public string folio_no { get; set; }
+        public int status_id { get; set; }
+        public string status_name { get; set; }
+        public string current_approver { get; set; }
+        public DateTime created_date { get; set; }
+        public string created_by { get; set; }
+        public string approver_name { get; set; }
+    }
+}

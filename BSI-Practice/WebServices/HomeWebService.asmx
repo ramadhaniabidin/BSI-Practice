5c250bce9ef7434a159060ab01d9c68f462bce7b
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-10-29 15:37:58 +0700

    add HomeWebService

---
+++ BSI-Practice/WebServices/HomeWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="HomeWebService.asmx.cs" Class="BSI_Practice.WebServices.HomeWebService" %>
---
+++ BSI-Practice/WebServices/HomeWebService.asmx.cs
@@ -0,0 +1,57 @@
+﻿using BSI_Logics.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace BSI_Practice.WebServices
+{
+    /// <summary>
+    /// Summary description for HomeWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [ScriptService]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class HomeWebService : System.Web.Services.WebService
+    {
+        private readonly HomeController controller = new HomeController();
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string GetRequestList(int current_login_role_id)
+        {
+            var returnedOutput = "";
+            try
+            {
+                var requestList = controller.GetRequestList(current_login_role_id);
+                var response = new
+                {
+                    Success = true,
+                    Message = "OK",
+                    ListItems = requestList
+                };
+                returnedOutput = new JavaScriptSerializer().Serialize(response);
+            }
+            catch(Exception ex)
+            {
+                var response = new
+                {
+                    Success = false,
+                    Message = $"Error = {ex.Message}"
+                };
+                returnedOutput = new JavaScriptSerializer().Serialize(response);
+            }
+            return returnedOutput;
+        }
+    }
+}

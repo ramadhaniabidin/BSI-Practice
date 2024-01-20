5867625eae6053936329ce852577c9fb142b159f
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2024-01-20 12:39:47 +0700

    update before pulling

--- BSI_Logics/Common/DatabaseManager.cs
+++ BSI_Logics/Common/DatabaseManager.cs
@@ -10,7 +10,7 @@ namespace BSI_Logics.Common
 {
     public class DatabaseManager
     {
-        public string sqlConnection = "Data Source=(localdb)\\myLocalDB;Initial Catalog=sample;Integrated Security=True;";
+        public string sqlConnection = "Data Source=103.90.248.49;Initial Catalog=BSI_POC;User ID = sa; Password=P@ssw0rd!23#";
         public SqlCommand cmd;
         public SqlDataReader dReader;
         public SqlTransaction trans;

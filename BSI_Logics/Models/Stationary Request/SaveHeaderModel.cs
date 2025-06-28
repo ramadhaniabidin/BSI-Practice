5e3cb8dab0ee8781615332635196598bf73b9047
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2025-06-28 18:55:43 +0700

    Add response models

---
+++ BSI_Logics/Models/Stationary Request/SaveHeaderModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSI_Logics.Models.Stationary_Request
+{
+    public class SaveHeaderModel: CommonResponseModel
+    {
+        public int InsertedID { get; set; }
+    }
+}

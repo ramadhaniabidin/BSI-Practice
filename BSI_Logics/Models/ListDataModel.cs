39cdeaa821a70d47ff2e680f2fcd8e2e56e75b1f
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2025-06-30 19:14:46 +0700

    Create ListDataModel.cs

---
+++ BSI_Logics/Models/ListDataModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSI_Logics.Models
+{
+    public class ListDataModel
+    {
+        public int ID { get; set; }
+        public string Transaction_Number { get; set; }
+        public string Requestor { get; set; }
+        public string Status { get; set; }
+        public string Current_Approver_Role { get; set; }
+        public string Current_Approver { get; set; }
+        public string Current_Approver_Email { get; set; }
+        public string CSS_Class { get; set; }
+        public int Total_Stage { get; set; }
+        public int Current_Stage { get; set; }
+    }
+}
